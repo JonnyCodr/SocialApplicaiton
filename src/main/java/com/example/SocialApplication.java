4ae33c254ba6a105fd8e3baaf12077e502a9a01f
michael <mlmmayhem@gmail.com>
2017-02-11 16:31:32 -0700

    added a user endpoint and a securoty cofiguration

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -1,12 +1,33 @@
 package com.example;
 
+import java.security.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class SocialApplication {
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.antMatcher("/**")
+				.authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**")
+				.permitAll()
+				.anyRequest()
+				.authenticated();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
