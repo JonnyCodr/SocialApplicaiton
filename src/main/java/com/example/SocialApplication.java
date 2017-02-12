9b23c4f7d1417bc0475b4cf4d6e5640fa3e2e0eb
michael <mlmmayhem@gmail.com>
2017-02-11 17:01:40 -0700

    added logout functionality.

--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.*;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.web.csrf.*;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
@@ -26,7 +27,9 @@ public class SocialApplication extends WebSecurityConfigurerAdapter {
 				.antMatchers("/", "/login**", "/webjars/**")
 				.permitAll()
 				.anyRequest()
-				.authenticated();
+				.authenticated()
+				.and().logout().logoutSuccessUrl("/").permitAll()
+				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 
 	public static void main(String[] args) {
